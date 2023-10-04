6c8c62cecc96e434f114a6290354c7a0df2dd10c
HasieEST <ayrtonm0@gmail.com>
2023-10-04 14:04:49 +0300

    Partial Bottom Navigation

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Splash from './src/screens/auth/Splash';
-import { colors } from './src/utils/colors';
-import Signin from './src/screens/auth/Signin';
-import Signup from './src/screens/auth/Signup'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import Config from "react-native-config"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Config from "react-native-config";
+
+import Splash from './src/screens/auth/Splash';
+import Signin from './src/screens/auth/Signin';
+import Signup from './src/screens/auth/Signup';
+
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
+
+import { colors } from './src/utils/colors';
+
 
 const theme = {
   colors: {
@@ -16,6 +24,25 @@ const theme = {
 }
 
 const Stack = createNativeStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
+
+const isSignedIn = true
+
+
+
+
+
 
 const App = () => {
   useEffect(() => {
@@ -30,9 +57,19 @@ const App = () => {
     <SafeAreaProvider>
       <NavigationContainer theme={theme}>
         <Stack.Navigator>
-          <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-          <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false }} />
-          <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+          {
+            isSignedIn ? (
+              <>
+                <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+              </>
+            ) : (
+              <>
+                <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+                <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false }} />
+                <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+              </>
+            )
+          }
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
